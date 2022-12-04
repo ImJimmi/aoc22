1487d923f64885994efcaeced1916a3e124cb889
James Johnson <james.johnson@focusrite.com>
2022-12-04 12:48:01 +0000

    Reduce repetition

--- day-4/main.ts
+++ day-4/main.ts
@@ -5,54 +5,40 @@ function range(from: number, to: number): number[] {
         .map(value => value + from);
 }
 
-function part1() {
-    const assignmentPairs = readLocalFile("assignment-pairs.txt", __dirname);
+type AssignmentPairs = number[][][];
+const assignmentPairs: AssignmentPairs = (
+    readLocalFile("assignment-pairs.txt", __dirname)
+        .trim()
+        .split("\n")
+        .map(line =>
+            line
+                .split(",")
+                .map(assignment =>
+                    assignment
+                        .split("-")
+                        .map(value => parseInt(value))
+                )
+                .map(assignment => range(
+                    Math.min(...assignment),
+                    Math.max(...assignment)
+                ))
+        )
+);
 
-    console.log(
+function countFullyContainedPairs(assignmentPairs: AssignmentPairs) {
+    return (
         assignmentPairs
-            .trim()
-            .split("\n")
-            .map(line =>
-                line
-                    .split(",")
-                    .map(assignment =>
-                        assignment
-                            .split("-")
-                            .map(value => parseInt(value))
-                    )
-                    .map(assignment => range(
-                        Math.min(...assignment),
-                        Math.max(...assignment)
-                    ))
-            )
             .filter(pair => {
                 return pair[0].every(value => pair[1].includes(value))
-                    || pair[1].every(value => pair[0].includes(value));
+                || pair[1].every(value => pair[0].includes(value));
             })
             .length
     );
 }
 
-function part2() {
-    const assignmentPairs = readLocalFile("assignment-pairs.txt", __dirname);
-
-    console.log(
+function countPartiallyContainedPairs(assignmentPairs: AssignmentPairs) {
+    return (
         assignmentPairs
-            .trim()
-            .split("\n")
-            .map(line =>
-                line
-                    .split(",")
-                    .map(assignment =>
-                        assignment
-                            .split("-")
-                            .map(value => parseInt(value))
-                    )
-                    .map(assignment => range(
-                        Math.min(...assignment),
-                        Math.max(...assignment)
-                    ))
-            )
             .filter(pair => {
                 return pair[0].some(value => pair[1].includes(value))
                     || pair[1].some(value => pair[0].includes(value));
@@ -61,5 +47,5 @@ function part2() {
     );
 }
 
-part1();
-part2();
+console.log(`Part 1: ${countFullyContainedPairs(assignmentPairs)} fully contained pairs.`);
+console.log(`Part 2: ${countPartiallyContainedPairs(assignmentPairs)} partially contained pairs.`);
